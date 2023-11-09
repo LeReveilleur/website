9d4eaa279d7c5fb1ae812e7a4bff9f13989d87ce
Chouffe <arthur@caillau.me>
2023-11-09 09:49:41 +0100

    [scripts] small cleanup

--- scripts/lereveilleur/core.clj
+++ scripts/lereveilleur/core.clj
@@ -4,11 +4,16 @@
    [lereveilleur.donors :as donors]))
 
 (def generate-donors-page! donors/generate-donors-page!)
-(def  download-youtube-thumbnails! video/download-youtube-thumbnails!)
+(def download-youtube-thumbnails! video/download-youtube-thumbnails!)
 (def validate-yaml-data-videos! video/validate-yaml-data-videos!)
-(def validate-yaml-data-videos! video/validate-yaml-data-videos!)
+(def validate-video-sources-markdown! video/validate-video-sources-markdown!)
 (def generate-video-posts! video/generate-video-posts!)
 (def clean-video-posts! video/clean-video-posts!)
 
 (comment
-  (generate-donors-page!))
+  (validate-yaml-data-videos!)
+  (validate-video-sources-markdown!)
+  (generate-donors-page!)
+  (download-youtube-thumbnails!)
+  (clean-video-posts!)
+  (generate-video-posts!))
